370d5129c62df666fbb26bee4a70c4df3642f95e
whattomine <whattomine.zhylee@mailhero.io>
2018-05-22 10:30:33 +0800

    initial commit

---
+++ kepl/config.js
@@ -0,0 +1,13 @@
+var api = "https://api.pool.whattomine.tech/kepl";
+
+var email = "whattomine.zhylee@mailhero.io";
+var telegram = "";
+var discord = "https://discord.gg/P2zPsW3";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-XMR"];
+
+var blockchainExplorer = "http://explorer.kepl.org/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.kepl.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
